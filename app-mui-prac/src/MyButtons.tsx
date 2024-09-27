0b32a2b10e32bd267f3d7d0b0d56cc3ca60a738b
yo16 <xxyo1xx@gmail.com>
2024-09-27 11:52:45 +0900

    add Stack

--- app-mui-prac/src/MyButtons.tsx
+++ app-mui-prac/src/MyButtons.tsx
@@ -1,32 +1,34 @@
-import { Button as MuiButton } from "@mui/material";
+import { Stack as MuiStack, Button as MuiButton } from "@mui/material";
 
 export const MyButtons: React.FC = () => {
     return (
         <>
-            <MuiButton
-                variant="contained"
-                color="primary"
-            >primary</MuiButton>
-            <MuiButton
-                variant="contained"
-                color="secondary"
-            >secondary</MuiButton>
-            <MuiButton
-                variant="contained"
-                color="info"
-            >info</MuiButton>
-            <MuiButton
-                variant="contained"
-                color="success"
-            >success</MuiButton>
-            <MuiButton
-                variant="contained"
-                color="error"
-            >error</MuiButton>
-            <MuiButton
-                variant="contained"
-                color="warning"
-            >warning</MuiButton>
+            <MuiStack direction="row" spacing={2} sx={{m:2, p:2}} >
+                <MuiButton
+                    variant="contained"
+                    color="primary"
+                >primary</MuiButton>
+                <MuiButton
+                    variant="contained"
+                    color="secondary"
+                >secondary</MuiButton>
+                <MuiButton
+                    variant="contained"
+                    color="info"
+                >info</MuiButton>
+                <MuiButton
+                    variant="contained"
+                    color="success"
+                >success</MuiButton>
+                <MuiButton
+                    variant="contained"
+                    color="error"
+                >error</MuiButton>
+                <MuiButton
+                    variant="contained"
+                    color="warning"
+                >warning</MuiButton>
+            </MuiStack>
         </>
     );
 }
