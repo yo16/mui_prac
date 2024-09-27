65f7683687f90e617f362edd254e9464c86ca24d
yo16 <xxyo1xx@gmail.com>
2024-09-27 12:19:14 +0900

    add reading dark-mode

--- app-mui-prac/src/App.tsx
+++ app-mui-prac/src/App.tsx
@@ -1,11 +1,17 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 import { MyButtons } from './MyButtons';
 
 export default function App() {
     // MUIのthemeを決定
-    const theme = createTheme();
+    const isDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+    const theme = createTheme({
+        palette: {
+            mode: isDarkMode ? "dark": "light"
+        }
+    });
 
     return (
         <ThemeProvider theme={theme}>
