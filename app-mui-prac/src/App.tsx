8f730027f1c7a603217eb179c649b880be27dd9e
yo16 <xxyo1xx@gmail.com>
2024-09-27 11:56:27 +0900

    add theme

--- app-mui-prac/src/App.tsx
+++ app-mui-prac/src/App.tsx
@@ -1,12 +1,16 @@
-import CssBaseline from '@mui/material/CssBaseline';
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import { MyButtons } from './MyButtons';
 
 export default function App() {
+    // MUIのthemeを決定
+    const theme = createTheme();
+
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <CssBaseline />
             <MyButtons />
-        </>
+        </ThemeProvider>
     )
 }
